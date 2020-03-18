1df07ba105594d3fcac5adcba310dc30a3612099
aoakua <aoakua@naver.com>
2020-03-18 09:14:49 +0900

    Updates

---
+++ precache-manifest.faba913c043789e3d32204dddfa04359.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d493a9c40dac703cd8564e26dc82c27c",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "ae9a184a4824f5be14a6",
+    "url": "/movie-app/static/css/main.12a45380.chunk.css"
+  },
+  {
+    "revision": "ed362dddb453e12db8c6",
+    "url": "/movie-app/static/js/2.c891ecc3.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie-app/static/js/2.c891ecc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9a184a4824f5be14a6",
+    "url": "/movie-app/static/js/main.6ed9f25c.chunk.js"
+  },
+  {
+    "revision": "8e52fcbe4b479b1a6971",
+    "url": "/movie-app/static/js/runtime-main.11c9c0e5.js"
+  }
+]);
